21cebad0131c6962956ed358b391e54dc423b4a5
Shubham <shubham76>
2017-09-16 01:09:21 +0530

    Completed the first project

--- SimpleServer/server.js
+++ SimpleServer/server.js
@@ -1,7 +1,54 @@
+//required modules
 var http = require('http');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello Http');
-});
-server.listen(8080);
+
+//Array of mimetypes
+var mimeType = {
+	"html" : "text/html",
+	"jpeg" : "image/jpeg",
+	"jpg" : "image/jpg",
+	"png" : "image/png",
+	"js" : "text/javascript",
+	"css" : "text/css"
+}
+
+//Create Server
+
+http.createServer(function (req, res) {
+	var uri = url.parse(req.url).pathname;
+	var fileName = path.join(process.cwd(),unescape(uri));
+	console.log('Loading'+uri);
+	var stats;
+
+	try {
+		stats = fs.lstatSync(fileName);
+	} catch(e) {
+		res.writeHead(404,{'contentType':'text/plain'});
+		res.write('404 not found.');
+		res.end();
+		return;
+		console.log(e);
+	}
+
+	//check if file or directory.
+
+	if(stats.isFile()){
+		var mimetype = mimeType[path.extname(fileName).split(".").reverse()[0]];
+		res.writeHead(200,{'contentType':mimetype});
+
+		var fileStream = fs.createReadStream(fileName);
+		fileStream.pipe(res);
+	}else if (stats.isDirectory()) {
+		res.writeHead(302,{
+			'Location' : 'index.html'
+		});
+		res.end();
+	} else{
+		res.writeHead(500,{'contentType':'text/plain'});
+		res.write('500 Internal Error!');
+		res.end();
+	}
+}).listen(3000);
